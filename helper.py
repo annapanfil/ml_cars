9310a7b559714b9b98e8ed6e632586364431ac72
Anna Panfil <55759533+panka134@users.noreply.github.com>
2021-09-26 22:29:06 +0200

    remove outliers from features
    remove outliers,
    merge some polish & english columns
    mess around

--- helper.py
+++ helper.py
@@ -8,8 +8,10 @@ import eli5
 def parse_price(val):
     return float(val.replace(" ", "").replace(",", "."))
 
-def get_df(df_train, df_test):
-    df_train = df_train[ df_train.index != 106447 ].reset_index(drop=True)
+def get_df(df_train, df_test):    
+    ### łączy dane treningowe i testowe, rozpakowuje słownik z danymi kategorycznymi
+    
+    df_train = df_train[ df_train.index != 106447 ].reset_index(drop=True)  # usuwamy jeden outlier
     df = pd.concat([df_train, df_test])
  
     params = df["offer_params"].apply(pd.Series)
@@ -18,6 +20,7 @@ def get_df(df_train, df_test):
     df = pd.concat([df, params], axis=1)
     print(df.shape)
 
+    # faktoryzujemy dane kategoryczne
     obj_feats = params.select_dtypes(object).columns
 
     for feat in obj_feats:
@@ -26,16 +29,20 @@ def get_df(df_train, df_test):
     return df
 
 def check_model(df, feats, model, cv=5, scoring="neg_mean_absolute_error", show_eli5=True):
+    ### dzieli dane na treningowe i testowe, przeprowadza cross validation 
+    
+    # podział danych
     df_train = df[ ~df["price_value"].isnull() ].copy()
     df_test = df[ df["price_value"].isnull() ].copy()
 
     X_train = df_train[feats]
     y_train = df_train["price_value"]
     
+    # testy
     scores = cross_val_score(model, X_train, y_train, cv=cv, scoring=scoring)
     result = np.mean(scores), np.std(scores)
     
-    if show_eli5:
+    if show_eli5: # pokaż dane do debugowania modelu
         model.fit(X_train, y_train)
         print(result)
         return eli5.show_weights(model, feature_names=feats)
@@ -43,23 +50,27 @@ def check_model(df, feats, model, cv=5, scoring="neg_mean_absolute_error", show_
     return result
 
 def check_log_model(df, feats, model, cv=5, scoring=mean_absolute_error, show_eli5=True):
+    ### dzieli dane na treningowe i testowe, przeprowadza cross validation dla cen zlogarytmizowanych
+    
     df_train = df[ ~df["price_value"].isnull() ].copy()
 
     X = df_train[feats]
     y = df_train["price_value"]
-    y_log = np.log(y)
+    y_log = np.log(y)  # zlogarytmizowanie wartości cen
     
-    cv = KFold(n_splits=cv, shuffle=True, random_state=0)
+    cv = KFold(n_splits=cv, shuffle=True, random_state=0) # podział na grupy (zwraca indeksy)
     scores = []
     for train_idx, test_idx in cv.split(X):
-        X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+        # wybór danych treningowych i testowych
+        X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]  
         y_log_train, y_test = y_log.iloc[train_idx], y.iloc[test_idx]
 
+        # trenowanie modelu
         model.fit(X_train, y_log_train)
         y_log_pred = model.predict(X_test)
-        y_pred = np.exp(y_log_pred)
+        y_pred = np.exp(y_log_pred) # powrót do zwykłych cen
 
-        score = scoring(y_test, y_pred)
+        score = scoring(y_test, y_pred)  # domyślnie mean absolute error
         scores.append(score)
         
     result = np.mean(scores), np.std(scores)
@@ -70,3 +81,8 @@ def check_log_model(df, feats, model, cv=5, scoring=mean_absolute_error, show_el
         return eli5.show_weights(model, feature_names=feats)
 
     return result
+
+def reset_outliers(df, feat, prc=99):
+    cut_value = np.percentile(df[feat], prc) # taką lub mniejszą pojemność ma 99% samochodów
+    
+    return df[feat].map(lambda x: x if x < cut_value else -1) # kasujemy tę wartość (outlier)
